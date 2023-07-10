3c1b78524937c9ac5046278a2658f37b11d5027e
Sobir Alimov <sobiriddin@gmail.com>
2023-07-10 19:12:26 +0500

    fix:query

--- src/entities/credit.ts
+++ src/entities/credit.ts
@@ -1,55 +1,54 @@
-import { Entity,BaseEntity,Column,PrimaryGeneratedColumn,ManyToOne,JoinColumn } from "typeorm";
+import { Entity, BaseEntity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from "typeorm";
 import { Client } from "./client";
 import { Product } from "./product";
 
 enum Status {
-    ACTIVE = 'active',
-    INACTIVE = 'inactive',
-    PENDING = 'pending',
-  }
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+  PENDING = 'pending',
+}
 
 @Entity('credit')
-export class Credit extends BaseEntity{
-    @PrimaryGeneratedColumn()
-    id:number
+export class Credit extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
 
-    @Column()
-    client_id:number;
-    @ManyToOne(() => Client, client => client.product,
-    {onDelete:"CASCADE"})
-    @JoinColumn({name:'client_id'})
-    client:Client
+  @Column()
+  client_id: number;
+  @ManyToOne(() => Client, client => client.product,
+    { onDelete: "CASCADE" })
+  @JoinColumn({ name: 'client_id' })
+  client: Client
 
-    @Column()
-    product_id:number;
-    @ManyToOne(() => Product, product => product.client,
-    {onDelete:"CASCADE"})
-    @JoinColumn({name:'product_id'})
-    product:Product
+  @Column()
+  product_id: number;
+  @ManyToOne(() => Product, product => product.client,
+    { onDelete: "CASCADE" })
+  @JoinColumn({ name: 'product_id' })
+  product: Product
 
-    @Column({
-      default:0,
-      nullable:true
-    })
-    client_deposit:number
- 
-    @Column()
-    deposit_amount:number
+  @Column({
+    nullable: true
+  })
+  client_deposit: number
 
-    @Column()
-    period:number;
+  @Column()
+  deposit_amount: number
 
-    @Column()
-    percent:number;
+  @Column()
+  period: number;
 
-    @Column({
-        type: 'enum',
-        enum: Status,
-        default: Status.PENDING,
-      })
-      status: Status;
+  @Column()
+  percent: number;
 
-    
-    @Column({ type: 'date', default: () => 'DATE(NOW())'})
-    duedate: Date;
+  @Column({
+    type: 'enum',
+    enum: Status,
+    default: Status.PENDING,
+  })
+  status: Status;
+
+
+  @Column({ type: 'date', default: () => 'DATE(NOW())' })
+  duedate: Date;
 }
--- src/queries/mainPageQueries.ts
+++ src/queries/mainPageQueries.ts
@@ -4,7 +4,7 @@ const totalCreditsSum = "SELECT SUM(credit.deposit_amount) AS total FROM credit"
 //Total sum with profit
 const totalWithProfit = "SELECT SUM(credit.deposit_amount) + SUM(deposit_amount*(percent%100) / 100) AS total_with_profit FROM credit"
 //Total profits from credits
-const totalProfit = "SELECT SUM(deposit_amount*(percent%100) / 100) AS total_profit FROM credit"
+const totalProfit = "SELECT SUM(deposit_amount*(percent%100) / 100) AS total_profit FROM credit WHERE credit.status = 'pending'";
 
 //Total expenses
 const totalExpenses = "SELECT SUM(amount) FROM expenses";
@@ -12,8 +12,8 @@ const totalExpenses = "SELECT SUM(amount) FROM expenses";
 
 const totalAfterExpenses = "SELECT (SELECT SUM(credit.deposit_amount) + SUM(deposit_amount*(percent%100) / 100) AS total_with_profit FROM credit) - (SELECT SUM(amount) FROM expenses) AS result"
 
-const creditsByMonthsQuery = "SELECT c.name AS client, p.name AS product, p.price AS price,ROUND((cr.deposit_amount +(cr.deposit_amount * (cr.percent::numeric % 100))/100) / cr.period,2) AS monthly_payment, generate_series(cr.duedate + 30, cr.duedate + ((cr.period) * '1 month'::interval), '1 month'::interval) AS date FROM credit cr JOIN client c ON cr.client_id = c.id JOIN product p ON cr.product_id = p.id WHERE cr.status = 'pending'"
+const creditsByMonthsQuery = "SELECT c.name AS client, p.name AS product, p.price AS price,ROUND((cr.deposit_amount +(cr.deposit_amount * (cr.percent::numeric % 100))/100) / cr.period,2) AS monthly_payment, generate_series(cr.duedate + 30, cr.duedate + ((cr.period) * '1 month'::interval), '1 month'::interval) AS date,cr.status as status FROM credit cr JOIN client c ON cr.client_id = c.id JOIN product p ON cr.product_id = p.id";
 
 
 
-module.exports = {totalCreditsSum,totalProfit,totalWithProfit,totalExpenses,creditsByMonthsQuery,totalAfterExpenses}
+module.exports = { totalCreditsSum, totalProfit, totalWithProfit, totalExpenses, creditsByMonthsQuery, totalAfterExpenses }
